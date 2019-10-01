18d523f6060283726223ffb0a7e6dd5d0f26f769
krkodil <ktassimir.koychev@gmail.com>
2019-10-01 12:00:17 +0300

    Code optimization

--- datecs.py
+++ datecs.py
@@ -11,7 +11,10 @@ NAK = 0x15
 SYN = 0x16
 TRM = 0x03
 
-CMD_PROGRAMMING = 0xff          # Programming {Name}<SEP>{Index}<SEP>{Value}<SEP>
+SEQ_START = 0x20
+SEQ_MAX = 0xff
+
+CMD_PROGRAMMING = 0xff          # X Only: Programming {Name}<SEP>{Index}<SEP>{Value}<SEP>
 CMD_GET_DIAGNOSTIC_INFO = 0x5a  # Diagnostic information
 
 CMD_GET_DATE_TIME = 0x3e        #
@@ -22,43 +25,60 @@ class Protocol(Enum):
     OLD = 1
     X = 2
 
+    @classmethod
+    def calc_bcc(cls, packet):
+        return sum(packet) & 0xffff
+
+    @classmethod
+    def encode_word(cls, w) -> bytearray:
+        b2 = w.to_bytes(2, "big")
+        b4 = bytearray()
+        b4.append(0x30 + (b2[0] >> 4))
+        b4.append(0x30 + (b2[0] & 0xf))
+        b4.append(0x30 + (b2[1] >> 4))
+        b4.append(0x30 + (b2[1] & 0xf))
+        return b4
+
+    def format_packet(self, seq, cmd, data) -> bytearray:
+        cmd_byte = cmd.to_bytes(1, "big")
+        seq_byte = seq.to_bytes(1, "big")
+
+        if self.value == 1:     # Protocol.OLD
+            packet_len = (0x24 + len(data)).to_bytes(1, "big")
+            packet = packet_len + seq_byte + cmd_byte + data + POSTAMBLE
+        else:                   # Protocol.X
+            packet_len = 0x002A + len(data)
+            packet = self.encode_word(packet_len) + seq_byte + self.encode_word(cmd) + data + POSTAMBLE
+
+        bcc = self.calc_bcc(packet)
+        return PREAMBLE + packet + self.encode_word(bcc) + TERMINATOR
+
+    def get_data(self, packet):
+        sep = packet.find(SEPARATOR)
+        if sep > 0:
+            if self.value == 1:  # Protocol.OLD
+                return packet[4:sep].decode()
+            else:                # Protocol.X
+                return packet[12:sep - 1].decode()
+        else:
+            return None
+
+
+class FiscalResponse:
+    def __init__(self, packet, protocol):
+        self.data = protocol.get_data(packet)
+        self.bcc = None
+        self.ok = False
+        self.status_bits = None
+        self.error_code = 0
+        self.error_message = ''
+
 
 class NakException(Exception):
     pass
 
 
-class FiscalResponse:
-    def __init__(self, packet, protocol):
-        sep = packet.find(SEPARATOR)
-        if sep > 0:
-            if protocol == Protocol.X:
-                self.data = packet[12:sep-1].decode()
-            else:
-                self.data = packet[4:sep].decode()
-        else:
-            self.data = None
-
-        self.bcc = None
-        self.ok = False
-        self.error_code = 0
-        self.error_message = ''
-
-
-class Connector:
-    def connect(self):
-        raise NotImplementedError
-
-    def write_data(self, data):
-        raise NotImplementedError
-
-    def read_data(self) -> bytearray:
-        raise NotImplementedError
-
-    def disconnect(self):
-        raise NotImplementedError
-
-
-class SerialConnector(Connector):
+class SerialConnector:
 
     def __init__(self, port, speed):
         self.port = port
@@ -98,7 +118,7 @@ class SerialConnector(Connector):
         self.com.close()
 
 
-class EthernetConnector(Connector):
+class EthernetConnector:
 
     def __init__(self, ip, port):
         self.ip = ip
@@ -107,12 +127,12 @@ class EthernetConnector(Connector):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        self.sock.settimeout(2.0)   # 2sec connection timeout
+        self.sock.settimeout(2.0)  # 2sec connection timeout
         self.sock.connect(self.address)
 
     def write_data(self, data):
         self.sock.sendall(data)
-        self.sock.settimeout(0.5)   # 500ms i/o timeout
+        self.sock.settimeout(0.5)  # 500ms i/o timeout
 
     def read_data(self) -> bytearray:
         response = bytearray()
@@ -135,44 +155,15 @@ class EthernetConnector(Connector):
         self.sock.close()
 
 
-def calc_bcc(packet):
-    return sum(packet) & 0xffff
-
-
-def encode_word(w) -> bytearray:
-    b2 = w.to_bytes(2, "big")
-    b4 = bytearray()
-    b4.append(0x30 + (b2[0] >> 4))
-    b4.append(0x30 + (b2[0] & 0xf))
-    b4.append(0x30 + (b2[1] >> 4))
-    b4.append(0x30 + (b2[1] & 0xf))
-    return b4
-
-
 class FiscalDevice:
 
     def __init__(self, connector, protocol=Protocol.X):
         self.connector = connector
         self.protocol = protocol
-        self.seq = 0x20
+        self.seq = SEQ_START
         self.last_packet = None
         self.last_packet = None
         self.connected = False
-
-    def format_packet(self, cmd, data) -> bytearray:
-        seq_byte = self.seq.to_bytes(1, "big")
-        packet_len = (0x24 + len(data)).to_bytes(1, "big")
-        cmd_byte = cmd.to_bytes(1, "big")
-        packet = packet_len + seq_byte + cmd_byte + data + POSTAMBLE
-        bcc = calc_bcc(packet)
-        return PREAMBLE + packet + encode_word(bcc) + TERMINATOR
-
-    def format_packet_x(self, cmd, data) -> bytearray:
-        seq_byte = self.seq.to_bytes(1, "big")
-        packet_len = 0x002A + len(data)
-        packet = encode_word(packet_len) + seq_byte + encode_word(cmd) + data + POSTAMBLE
-        bcc = calc_bcc(packet)
-        return PREAMBLE + packet + encode_word(bcc) + TERMINATOR
 
     def connect(self):
         self.connector.connect()
@@ -185,32 +176,28 @@ class FiscalDevice:
         return True
 
     def execute(self, cmd, data=b''):
-
         if not self.connected:
             raise Exception('Not connected')
 
-        if self.seq > 0xff:
-            self.seq = 0x20
+        if self.seq >= SEQ_MAX:
+            self.seq = SEQ_START
         else:
             self.seq += 1
 
-        if self.protocol == Protocol.OLD:
-            self.last_packet = self.format_packet(cmd, data)
-        else:
-            self.last_packet = self.format_packet_x(cmd, data)
+        self.last_packet = self.protocol.format_packet(self.seq, cmd, data)
 
-        self.connector.write_data(self.last_packet)         # send
+        self.connector.write_data(self.last_packet)      # send cmd
         try:
-            response = self.connector.read_data()
-        except NakException:                                # NAK from ECR
-            self.connector.write_data(self.last_packet)     # repeat last cmd with same seq
-            response = self.connector.read_data()
+            response_data = self.connector.read_data()
+        except NakException:                             # NAK from ECR
+            self.connector.write_data(self.last_packet)  # repeat last cmd with same seq
+            response_data = self.connector.read_data()
 
-        return FiscalResponse(response, self.protocol)
+        return FiscalResponse(response_data, self.protocol)
 
 
 if __name__ == '__main__':
-    fd = FiscalDevice(EthernetConnector('192.168.8.100', 4999), Protocol.X)
+    fd = FiscalDevice(EthernetConnector('192.168.0.36', 4999), Protocol.X)
     if fd.connect():
         try:
             print('\nEthernet connected')
