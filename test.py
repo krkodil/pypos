2db6d0124f57df0e87b5423bc25e20daf5eddf38
krkoychev <krassimir.koychev@gmail.com>
2019-10-07 19:41:40 +0300

    set date time

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from ecr import DatecsFiscalDevice
 from protocol import DatecsProtocol
 from connector import EthernetConnector
+from datetime import datetime
 
 
 if __name__ == '__main__':
     fd = DatecsFiscalDevice(EthernetConnector('192.168.0.36', 4999), DatecsProtocol.X)
     if fd.connect():
         try:
+            fd.set_date_time(datetime.now())
             print(fd.get_date_time())
         finally:
             fd.disconnect()
